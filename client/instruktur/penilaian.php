fa5b6c984a264325c3e00fb7bd61c0fcaf54a627
aryawildhani13 <aryawildhaniputra@gmail.com>
2023-04-02 21:28:27 +0700

    penilaian

---
+++ client/instruktur/penilaian.php
@@ -0,0 +1,61 @@
+<?php
+include_once('../template/header.php');
+include_once('../../api/auth/access_control.php');
+user_access('instruktur');
+
+$sql = "SELECT * FROM penilaian";
+$result = $db->query($sql) or die($sql);
+$result->fetch_assoc();
+?>
+
+<div id="jenjang" class="w-full min-h-screen flex">
+    <?php include_once '../components/dashboard_sidebar.php' ?>
+    <div class="w-full flex flex-col">
+        <div class="p-4 sm:ml-64">
+            <?php include_once '../components/dashboard_navbar.php'; ?>
+
+            <div class="flex items-center gap-5">
+                <h4 class="my-7 font-semibold text-gray-800 dark:text-white">Penilaian</h4>
+            </div>
+
+            <?php generate_breadcrumb([['title' => 'Penilaian', 'filename' => 'penilaian.php']]); ?>
+            <div class="relative overflow-x-auto mt-5">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+
+                    <a>
+                        <tr>
+                            <th scope="col" class="px-6 py-3"></th>
+                            <th scope="col" class="px-6 py-3"></th>
+                            <th scope="col" class="px-6 py-3"></th>
+                            <th scope="col" class="px-6 py-3"></th>
+                            <th scope="col" class="px-6 py-3">Mata Pelajaran</th>
+                            <th scope="col" class="px-6 py-3"></th>
+                            <th scope="col" class="px-6 py-3"></th>
+                        </tr>
+                    </a>
+                       
+                    </thead>
+                    <tbody>
+                        <?php foreach ($result as $key => $value) : ?>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th class="px-6 py-4 text-amber-500"><?= $key + 1 ?></th>
+                                <td class="px-6 py-4"><?= $value['judul_penilaian'] ?></td>
+                                <td class="px-6 py-4"><?= $value['keterangan_penilaian'] ?></td>
+                                <td class="px-6 py-4"></td>
+                                <td class="px-6 py-4"></td>
+                                <td class="px-6 py-4"></td>
+                                <td class="px-6 py-4"><?= $value['tgl_penilaian'] ?></td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php
+$result->free_result();
+include_once('../template/footer.php') ?>
