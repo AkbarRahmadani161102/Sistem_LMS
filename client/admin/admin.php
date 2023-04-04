80f7dbb96d0bebc59258977865510cd8f7ad19dd
evanightly <adamhenderson3x3@gmail.com>
2023-04-04 11:57:01 +0700

    Update edit kelas UI

--- client/admin/admin.php
+++ client/admin/admin.php
@@ -171,7 +171,7 @@ if (isset($_GET['edit'])) {
     </div>
 </div>
 
-<div id="add_admin_modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+<div id="add_admin_modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
     <div class="relative w-full h-full max-w-7xl md:h-auto">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
