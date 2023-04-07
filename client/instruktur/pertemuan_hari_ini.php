c64b02804b33bb88ce7a465d369b7781bb0a5097
AkbarRahmadani161102 <akbarrahmadani161102@gmail.com>
2023-04-07 21:00:16 +0700

    Update api pertemuan_hari_ini

--- client/instruktur/pertemuan_hari_ini.php
+++ client/instruktur/pertemuan_hari_ini.php
@@ -153,11 +153,10 @@ if (isset($_GET['presence'])) {
                                                         <input id="kehadiran<?= $key ?>" type="radio" value="T" name="kehadiran['<?= $siswa['id_siswa']?>']" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                                     </td>
                                                 </tr>
-                                                <input type="hidden" name="id_siswa[]" value="<?= $siswa['id_siswa'] ?>">
                                             <?php endforeach ?>
                                         </tbody>
                                     </table>
-                                    <button type="submit" name="presence" class="block ml-auto mt-5 px-4 py-2 bg-green-300 rounded">Selesai Absensi</button>
+                                    <button type="submit" name="presence" value="<?= $_GET['presence']?>" class="block ml-auto mt-5 px-4 py-2 bg-green-300 rounded">Selesai Absensi</button>
                                 </div>
                             </div>
                         </form>
