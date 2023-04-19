ab79046abe672a432e4acc7d7d598d27a547f55e
evanightly <adamhenderson3x3@gmail.com>
2023-04-19 18:51:36 +0700

    Patch pengajuan required input (siswa)

--- client/siswa/pengajuan.php
+++ client/siswa/pengajuan.php
@@ -70,14 +70,14 @@ $data_tunggakan = $db->query($sql);
                 <!-- Modal body -->
                 <div class="modal__body">
                     <label class="block" for="judul">Judul</label>
-                    <input id="judul" class="border rounded w-full py-1.5 px-2 border-gray-400 mt-1" name="judul" />
+                    <input id="judul" class="border rounded w-full py-1.5 px-2 border-gray-400 mt-1" name="judul" required />
 
                     <label for="keterangan" class="block">Keterangan</label>
                     <textarea name="keterangan" id="keterangan" rows="4" class="border rounded w-full py-1.5 px-2 border-gray-400 mt-1" placeholder="Write your thoughts here..."></textarea>
                 </div>
                 <!-- Modal footer -->
                 <div class="modal__footer">
-                    <button data-modal-hide="add-pengajuan-modal" name="create" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    <button name="create" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                     <button data-modal-hide="add-pengajuan-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Close</button>
                 </div>
             </form>
