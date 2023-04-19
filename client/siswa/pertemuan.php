39db3633b60386a18cb8de55a2e2f351df4901ed
evanightly <adamhenderson3x3@gmail.com>
2023-04-19 18:27:59 +0700

    Update handle pergantian instruktur (siswa)

--- client/siswa/pertemuan.php
+++ client/siswa/pertemuan.php
@@ -32,7 +32,7 @@ if (isset($_GET['pergantian_instruktur'])) {
     $data_instruktur = $db->query($sql) or die($db->error);
     $data_instruktur->fetch_assoc();
 } else {
-    $sql = "SELECT j.*, dj.*, dj.id_detail_jadwal, i.nama nama_instruktur, m.nama nama_mapel, dj.tgl_pertemuan tgl 
+    $sql = "SELECT j.*, dj.*, dj.id_detail_jadwal, i.nama nama_instruktur, m.nama nama_mapel, dj.tgl_pertemuan tgl, k.id_ketua_kelas
     FROM jadwal j 
     JOIN kelas k on j.id_kelas = k.id_kelas
     JOIN detail_kelas dk on dk.id_kelas = k.id_kelas
@@ -41,9 +41,11 @@ if (isset($_GET['pergantian_instruktur'])) {
     JOIN detail_jadwal dj on dj.id_jadwal = j.id_jadwal 
     JOIN instruktur i on i.id_instruktur = dj.id_instruktur
     WHERE s.id_siswa = '$id_siswa' AND j.id_jadwal = dj.id_jadwal
-    GROUP BY j.id_mapel";
+    ORDER BY tgl";
     $data_pertemuan = $db->query($sql) or die($db->error);
     $data_pertemuan->fetch_assoc();
+
+    $ketua_kelas =  $data_pertemuan->fetch_assoc()['id_ketua_kelas'] === $id_siswa;
 }
 ?>
 
@@ -56,34 +58,36 @@ if (isset($_GET['pergantian_instruktur'])) {
                 <?php generate_breadcrumb([['title' => 'Pertemuan', 'filename' => 'pertemuan.php'], ['title' => 'Pergantian Instruktur', 'filename' => '#']]); ?>
                 <h4 class="my-7 font-semibold text-gray-800 dark:text-white">Pergantian Instruktur</h4>
 
-                <div class="flex flex-1 flex-wrap text-gray-800 dark:text-white rounded-lg bg-gray-200 dark:bg-gray-700 p-5">
-                    <div class="flex w-fit flex-col space-y-4 border-r-2 pr-5">
-                        <div class="flex flex-col">
-                            <h6 class="font-semibold">Mapel:</h6>
-                            <p><?= $nama_mapel ?></p>
+                <?php if ($ketua_kelas) : ?>
+                    <div class="flex flex-1 flex-wrap text-gray-800 dark:text-white rounded-lg bg-gray-200 dark:bg-gray-700 p-5">
+                        <div class="flex w-fit flex-col space-y-4 border-r-2 pr-5">
+                            <div class="flex flex-col">
+                                <h6 class="font-semibold">Mapel:</h6>
+                                <p><?= $nama_mapel ?></p>
+                            </div>
+                            <div class="flex flex-col">
+                                <h6 class="font-semibold">Instruktur:</h6>
+                                <p><?= $nama_instruktur ?></p>
+                            </div>
                         </div>
-                        <div class="flex flex-col">
-                            <h6 class="font-semibold">Instruktur:</h6>
-                            <p><?= $nama_instruktur ?></p>
-                        </div>
-                    </div>
-                    <div class="flex flex-1 flex-col space-y-4">
-                        <form action="../../api/siswa/pertemuan.php" method="post" class="form pl-5 flex flex-1 flex-col items-start justify-between">
-                            <label for="instruktur">Pilih Instruktur Pengganti</label>
-                            <select class="selectize input" name="instruktur" id="instruktur" required>
-                                <?php foreach ($data_instruktur as $instruktur) : ?>
-                                    <option value="<?= $instruktur['id_instruktur'] ?>|<?= $instruktur['nama'] ?>"><?= $instruktur['nama'] ?></option>
-                                <?php endforeach ?>
-                            </select>
+                        <div class="flex flex-1 flex-col space-y-4">
+                            <form action="../../api/siswa/pertemuan.php" method="post" class="form pl-5 flex flex-1 flex-col items-start justify-between">
+                                <label for="instruktur">Pilih Instruktur Pengganti</label>
+                                <select class="selectize input" name="instruktur" id="instruktur" required>
+                                    <?php foreach ($data_instruktur as $instruktur) : ?>
+                                        <option value="<?= $instruktur['id_instruktur'] ?>|<?= $instruktur['nama'] ?>"><?= $instruktur['nama'] ?></option>
+                                    <?php endforeach ?>
+                                </select>
 
-                            <input type="hidden" name="id_siswa" value="<?= $id_siswa ?>">
-                            <input type="hidden" name="kelas" value="<?= $nama_kelas ?>">
-                            <input type="hidden" name="mapel" value="<?= $nama_mapel ?>">
-                            <input type="hidden" name="tgl_pertemuan" value="<?= $tgl_pertemuan ?>">
-                            <button name="ajukan_instruktur" type="submit" class="btn" value="<?= $id_detail_jadwal ?>">Ajukan Instruktur Pengganti</button>
-                        </form>
+                                <input type="hidden" name="id_siswa" value="<?= $id_siswa ?>">
+                                <input type="hidden" name="kelas" value="<?= $nama_kelas ?>">
+                                <input type="hidden" name="mapel" value="<?= $nama_mapel ?>">
+                                <input type="hidden" name="tgl_pertemuan" value="<?= $tgl_pertemuan ?>">
+                                <button name="ajukan_instruktur" type="submit" class="btn" value="<?= $id_detail_jadwal ?>">Ajukan Instruktur Pengganti</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                <?php endif ?>
             <?php else : ?>
                 <?php generate_breadcrumb([['title' => 'Pertemuan', 'filename' => 'pertemuan.php']]); ?>
                 <h4 class="my-7 font-semibold text-gray-800 dark:text-white">Pertemuan Kelas</h4>
@@ -102,7 +106,7 @@ if (isset($_GET['pergantian_instruktur'])) {
                         </thead>
                         <tbody>
                             <?php foreach ($data_pertemuan as $key => $pertemuan) : ?>
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <tr class="border-b bg-white dark:bg-gray-800 dark:border-gray-700">
                                     <th class="px-6 py-4 text-amber-500"><?= $key + 1 ?></th>
                                     <td class="px-6 py-4"><?= $pertemuan['nama_mapel'] ?></td>
                                     <td class="px-6 py-4"><?= $pertemuan['nama_instruktur'] ?></td>
@@ -110,10 +114,16 @@ if (isset($_GET['pergantian_instruktur'])) {
                                     <td class="px-6 py-4"><?= $pertemuan['jam_mulai'] ?></td>
                                     <td class="px-6 py-4"><?= $pertemuan['jam_selesai'] ?></td>
                                     <td class="px-6 py-4">
-                                        <?php if ($pertemuan['status_kehadiran_instruktur'] !== "Proses Pergantian") : ?>
-                                            <a href="?pergantian_instruktur=<?= $pertemuan['id_detail_jadwal'] ?>" class="btn btn--outline-amber">Ajukan Pergantian Instruktur</a>
+                                        <?php if ($pertemuan['tgl'] <= date('Y-m-d')) : ?>
+                                            <p class="text-red-500">Pertemuan telah usai</p>
                                         <?php else : ?>
-                                            <p class="text-amber-500">Pergantian instruktur sedang dalam proses</p>
+                                            <?php if ($ketua_kelas) : ?>
+                                                <?php if ($pertemuan['status_kehadiran_instruktur'] !== "Proses Pergantian") : ?>
+                                                    <a href="?pergantian_instruktur=<?= $pertemuan['id_detail_jadwal'] ?>" class="btn btn--outline-amber">Ajukan Pergantian Instruktur</a>
+                                                <?php else : ?>
+                                                    <p class="text-amber-500">Pergantian instruktur sedang dalam proses</p>
+                                                <?php endif ?>
+                                            <?php endif ?>
                                         <?php endif ?>
                                     </td>
                                 </tr>
