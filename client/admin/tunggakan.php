c78a97d3bccec166d77d8c9250ad7c892140c771
evanightly <adamhenderson3x3@gmail.com>
2023-05-04 19:12:50 +0700

    Disable datatable paging

--- client/admin/tunggakan.php
+++ client/admin/tunggakan.php
@@ -87,7 +87,7 @@ if (isset($_GET['tahun']) && isset($_GET['bulan'])) {
 
             <?php if (isset($_GET['tahun']) && isset($_GET['bulan'])) : ?>
                 <div class="relative overflow-x-auto">
-                    <table class="datatable w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <table class="datatable-disable-paging w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3"></th>
