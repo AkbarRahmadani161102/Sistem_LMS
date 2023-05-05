28190c85bf3e2f8968dede9dafae3d6b463967da
evanightly <adamhenderson3x3@gmail.com>
2023-05-05 15:01:22 +0700

    Revisi gaji instruktur

--- api/admin/gaji_instruktur.php
+++ api/admin/gaji_instruktur.php
@@ -1,15 +1,19 @@
 <?php
 include_once '../util/db.php';
 if (isset($_POST['sync'])) {
-    $month = date('m');
+    $tahun = $_POST['tahun'];
+    $bulan = $_POST['bulan'];
+    $hari = 10;
+    $tanggal_trigger = "$tahun-$bulan-$hari";
+    $tgl_dibuat = date('Y-m-t', strtotime($tanggal_trigger));
 
     $sql = "SELECT dj.id_instruktur, SUM(biaya_per_pertemuan) nominal FROM detail_jadwal dj
     JOIN jadwal j ON dj.id_jadwal = j.id_jadwal
     JOIN kelas k ON j.id_kelas = j.id_kelas
     JOIN jenjang je ON k.id_jenjang = je.id_jenjang
-    WHERE MONTH(tgl_pertemuan) = $month
+    WHERE MONTH(tgl_pertemuan) = $bulan AND YEAR(tgl_pertemuan) = $tahun
     AND dj.status_kehadiran_instruktur = 'Hadir'
-    AND dj.id_instruktur NOT IN (SELECT id_instruktur FROM gaji WHERE MONTH(tgl_pertemuan) = $month)
+    AND dj.id_instruktur NOT IN (SELECT id_instruktur FROM gaji WHERE MONTH(tgl_dibuat) = $bulan AND YEAR(tgl_dibuat) = $tahun)
     GROUP BY dj.id_instruktur";
 
     $data_instruktur = $db->query($sql);
@@ -17,7 +21,7 @@ if (isset($_POST['sync'])) {
     foreach ($data_instruktur as $value) {
         $id_instruktur = $value['id_instruktur'];
         $nominal = $value['nominal'];
-        $sql = "INSERT INTO gaji (id_instruktur, nominal) VALUES ('$id_instruktur', '$nominal')";
+        $sql = "INSERT INTO gaji (id_instruktur, nominal, tgl_dibuat) VALUES ('$id_instruktur', '$nominal', '$tgl_dibuat')";
         $db->query($sql);
     }
     $_SESSION['toast'] = ['icon' => 'success', 'title' => 'Data berhasil disinkronkan', 'icon_color' => 'greenlight'];
@@ -26,9 +30,22 @@ if (isset($_POST['update'])) {
     $id_gaji = $_POST['update'];
     $tgl_penerimaan = $_POST['tgl_penerimaan'];
 
-    $sql = "UPDATE gaji SET tgl_penerimaan = '$tgl_penerimaan' WHERE id_gaji = '$id_gaji'";
+    if ($tgl_penerimaan === 'NULL')
+        $sql = "UPDATE gaji SET tgl_penerimaan = NULL WHERE id_gaji = '$id_gaji'";
+    else
+        $sql = "UPDATE gaji SET tgl_penerimaan = '$tgl_penerimaan' WHERE id_gaji = '$id_gaji'";
+
     $db->query($sql);
     $_SESSION['toast'] = ['icon' => 'success', 'title' => 'Data berhasil diubah', 'icon_color' => 'greenlight'];
 }
+if (isset($_POST['redirect'])) {
+    $tahun = $_POST['tahun'];
+    $bulan = $_POST['bulan'];
 
-redirect('../../client/admin/gaji_instruktur.php');
+    redirect("../../client/admin/gaji_instruktur.php?tahun=$tahun&bulan=$bulan");
+}
+?>
+
+<script>
+    history.back()
+</script>
