a978e8df62d0d607e732690777ef0ea77397a6a3
evanightly <adamhenderson3x3@gmail.com>
2023-05-15 19:05:30 +0700

    Implement Clear Session Timeout (Archived)

--- config.php
+++ config.php
@@ -23,3 +23,11 @@ if ($is_production()) {
 } else {
     define('ROOT_PATH', dirname(__FILE__) . '/');
 }
+
+// Implement Clear Session Timeout
+// if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+//     // last request was more than 30 minutes ago
+//     session_unset();     // unset $_SESSION variable for the run-time 
+//     session_destroy();   // destroy session data in storage
+// }
+// $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
