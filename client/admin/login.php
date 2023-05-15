5962eed04ba92e111ba9239d8b18e74019a5ba54
evanightly <adamhenderson3x3@gmail.com>
2023-05-15 19:20:04 +0700

    Add button kembali (Login Admin)

--- client/admin/login.php
+++ client/admin/login.php
@@ -15,8 +15,11 @@
                     <label for="password" class="text-secondary text-gray-400 dark:text-white">Password</label>
                     <input type="password" class="input" id="password" name="password" required>
                 </div>
-                <input type="hidden" name="role" value="admin">
-                <button type="submit" class="btn w-full bg-amber-500 dark:bg-amber-500 dark:text-white" name="login">Login</button>
+                <div class="flex flex-col gap-5 pt-4">
+                    <input type="hidden" name="role" value="admin">
+                    <button type="submit" class="btn w-full bg-amber-500 dark:bg-amber-500 dark:text-white" name="login">Login</button>
+                    <a href="../../index.php" class="btn text-center">Kembali</a>
+                </div>
             </form>
             <p class="text-center text-gray-400 dark:text-white">Untuk mendaftar silahkan ajukan pendaftaran <span class="text-amber-500">onsite</span></p>
         </div>
