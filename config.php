07c3d106e5a268478159ac2c8b86ccf0188243df
evanightly <adamhenderson3x3@gmail.com>
2023-05-31 20:11:06 +0700

    Update Reset Session

--- config.php
+++ config.php
@@ -25,9 +25,9 @@ if ($is_production()) {
 }
 
 // Implement Clear Session Timeout
-// if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
-//     // last request was more than 30 minutes ago
-//     session_unset();     // unset $_SESSION variable for the run-time 
-//     session_destroy();   // destroy session data in storage
-// }
-// $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
